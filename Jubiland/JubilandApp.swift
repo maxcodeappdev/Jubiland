52681e7b0497f795058e97d25d5611c7be0bae44
Max Contreras <maxcodeappdev@gmail.com>
2025-02-25 20:36:57 -0700

    Initial Commit

---
+++ Jubiland/JubilandApp.swift
@@ -0,0 +1,17 @@
+//
+//  JubilandApp.swift
+//  Jubiland
+//
+//  Created by Max Contreras on 2/25/25.
+//
+
+import SwiftUI
+
+@main
+struct JubilandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
